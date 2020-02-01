7a7a894c2a385f8557171f6de1c1fdbcb8a2ca11
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2020-02-01 12:09:34 +0200

    Implement ChatManager and rename GameManager to DialogueManager.

---
+++ Assets/_Game/Scripts/DialogueManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance { get; private set; }
+
+    public Dialogue currentDialogue;
+    [SerializeField] private GameObject[] options;
+    [SerializeField] private GameObject endScreen;
+    [SerializeField] private List<CodeEmojiPair> emojis;
+    private int score = 3;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayDialogue()
+    {
+        DialogueNode node = currentDialogue.GetNextNode();
+        if (node == null)
+        {
+            // Show end screen
+        }
+        if (node.type == DialogueType.Normal)
+        {
+            ShowText(node);
+        }
+        else if (node.type == DialogueType.Option)
+        {
+            ShowOptions(node);
+        }
+    }
+
+    public void ShowText(DialogueNode node)
+    {
+        List<Sprite> nodeEmojis = ParseEmojis(node.text);
+        ChatManager.Instance.AddBubble(nodeEmojis);
+    }
+
+    private List<Sprite> ParseEmojis(string text)
+    {
+        List<Sprite> parsed = new List<Sprite>();
+        string[] codes = text.Split(' ');
+        for (int i = 0; i < codes.Length; i++)
+        {
+            CodeEmojiPair emoji = emojis.Find(x => x.code == codes[i]);
+            if (emoji != null)
+            {
+                parsed.Add(emoji.sprite);
+            }
+            else
+            {
+                Debug.LogError("No matching emoji for code " + codes[i]);
+            }
+        }
+        return parsed;
+    }
+
+    public void ShowOptions(DialogueNode node)
+    {
+        for (int i = 0; i < node.options.Length; i++)
+        {
+            options[i].SetActive(true);
+        }
+    }
+
+    public void HideOptions()
+    {
+        foreach(GameObject option in options)
+        {
+            option.SetActive(false);
+        }
+    }
+
+    public void DoPositiveEffect()
+    {
+        score += 2;
+        OnOptionSelected();
+    }
+
+    public void DoNeutralEffect()
+    {
+        score++;
+        OnOptionSelected();
+    }
+
+    public void DoNegativeEffect()
+    {
+        OnOptionSelected();
+    }
+
+    public void OnOptionSelected()
+    {
+        HideOptions();
+        PlayDialogue();
+    }
+}
+
+[Serializable]
+public class CodeEmojiPair
+{
+    public string code;
+    public Sprite sprite;
+}
--- Assets/_Game/Scripts/UI/ChatManager.cs
+++ Assets/_Game/Scripts/UI/ChatManager.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class ChatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ChatManager Instance { get; private set; }
+
+    [SerializeField] private GameObject bubblePrefab;
+    [SerializeField] private int maxBubbles;
+    private List<Bubble> activeBubbles = new List<Bubble>();
+    private Queue<Bubble> inactiveBubbles = new Queue<Bubble>();
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for (int i = 0; i < maxBubbles; i++)
+        {
+            GameObject newBubble = Instantiate(bubblePrefab, Vector3.zero, Quaternion.identity);
+            Bubble script = newBubble.GetComponent<Bubble>();
+            inactiveBubbles.Enqueue(script);
+        }
+    }
+
+    public void AddBubble(List<Sprite> emojis)
+    {
+        Bubble bubble = inactiveBubbles.Dequeue();
+        bubble.Initialize(emojis, Vector3.zero, 0);
+        // Move others forward
+        foreach(Bubble activeBubble in activeBubbles)
+        {
+            // activeBubble.MoveUp()
+        }
+        activeBubbles.Add(bubble);
+        // Clear last bubble
+        if (activeBubbles.Count >= maxBubbles)
+        {
+            ClearBubble(activeBubbles[activeBubbles.Count - 1]);
+        }
+    }
+
+    public void ClearBubble(Bubble bubble)
+    {
+        // bubble.FadeOut();
+        activeBubbles.Remove(bubble);
+        inactiveBubbles.Enqueue(bubble);
+    }
+
+    public void ClearBubbles()
+    {
+        foreach(Bubble bubble in activeBubbles)
+        {
+            ClearBubble(bubble);
+        }
     }
 }
