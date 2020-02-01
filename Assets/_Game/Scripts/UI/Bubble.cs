ed2d6d88dbd59ffe9af9cfc1284f60200459100e
Sade Sirén <fyrwill@gmail.com>
2020-02-01 12:20:06 +0200

    - muutama placeholder emoji - bubble-scriptissä animaatiot - aloitettu laittamaan chattiä world space kanvaasille

--- Assets/_Game/Scripts/UI/Bubble.cs
+++ Assets/_Game/Scripts/UI/Bubble.cs
@@ -1,31 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Image))]
 public class Bubble : MonoBehaviour
 {
-    private SpriteRenderer sprite;
-    private List<Sprite> emojis;
+    private List<Image> emojis;
     private int counter;
     private bool animating = false;
 
     // adjustable values
     [SerializeField] private float animationTime = 0.5f;
-    [SerializeField] private Sprite initBubble;
-    [SerializeField] private Sprite regularBubble;
+    [SerializeField] private Image initBubble;
+    [SerializeField] private Image regularBubble;
+    [SerializeField] private Vector3 initEmojiPosition;
 
-    void Start() 
-    {
-        sprite = gameObject.GetComponent<SpriteRenderer>();
-    }
-
-    public void Initialize(List<Sprite> sprites, Vector3 startPos, float xOffset)
+    public void Initialize(List<Image> sprites, Vector3 startPos, float xOffset)
     {
         emojis = sprites;
         counter = emojis.Count;
         gameObject.transform.position = startPos;
+        FadeIn();
     }
 
+    private void TypewriteEmojis()
+    {
 
+    }
+
+    public void MoveUp(float xOffset)
+    {
+        StartCoroutine(MoveAnimation(xOffset));
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeAnimation(true));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeAnimation(false));
+    }
+
+    IEnumerator FadeAnimation(bool fadein)
+    {
+        // setup
+        Vector3 origScale = transform.localScale; // should be (1,1,1)
+        if(fadein)
+        {
+            // make it invisibly small
+            transform.localScale = new Vector3(0,0,0);
+        }
+        float scalingFactor = 0;
+        float timer = 0;
+        while(timer < animationTime)
+        {
+            timer = Mathf.Clamp(timer + Time.unscaledDeltaTime, 0, animationTime);
+
+            if(fadein)
+            {
+                // TODO make this animationcurve
+                scalingFactor = Mathf.Lerp(0, origScale.x, timer / animationTime);
+            }
+            else
+            {
+                scalingFactor = Mathf.Lerp(origScale.x, 0, timer / animationTime);
+            }
+            yield return null;
+        }
+        if(!fadein)
+        {
+            // The object should be moved to unused list
+            Debug.Log("Done FadeOut");
+        }
+    }
+
+    IEnumerator MoveAnimation(float xOffset)
+    {
+        // setup
+        Vector3 outPosition = transform.position + new Vector3(xOffset, 0, 0);
+        Debug.Log("Going to position " + outPosition);
+
+        // lerp the object's position
+        float timer = 0;
+        while(timer < animationTime)
+        {
+            timer = Mathf.Clamp(timer + Time.unscaledDeltaTime, 0, animationTime);
+
+            float xPos = Mathf.Lerp(gameObject.transform.position.x, outPosition.x, timer / animationTime);
+
+            gameObject.transform.position += new Vector3(xPos, 0, 0);
+
+            yield return null;
+        }
+    }
 }
