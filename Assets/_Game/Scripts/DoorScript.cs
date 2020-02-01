fd728d8a03dfb97d974abd16a8d93010ef9b0e05
Matti Mänty <mazena86@gmail.com>
2020-02-01 20:53:57 +0200

    Added door animation script

---
+++ Assets/_Game/Scripts/DoorScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curve;
+
+    [SerializeField] float closedAngle;
+    [SerializeField] float openAngle;
+
+    bool open = false;
+    float animationDuration = 0.75f;
+
+    public void OperateDoor()
+    {
+        StartCoroutine(OperateDoorRoutine());
+    }
+
+    IEnumerator OperateDoorRoutine()
+    {
+        float timer = 0f;
+        float startAngle = transform.rotation.z;
+        float targetAngle = open ? closedAngle : openAngle;
+
+        while(timer < animationDuration)
+        {
+            timer = Mathf.Clamp(timer + Time.deltaTime, 0, animationDuration);
+            float rotationAngle = Mathf.Lerp(startAngle, targetAngle, curve.Evaluate(timer / animationDuration));
+            transform.rotation = Quaternion.Euler(0, 0, rotationAngle);
+            yield return null;
+        }
+
+        open = !open;
+    }
+
+}
