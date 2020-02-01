e43fc3d7f8b5e0d453b7f15bf0239a3ff31e4c80
Sade Sirén <fyrwill@gmail.com>
2020-02-01 11:15:51 +0200

    skeleton Bubble ja Chatmanager nostettu backend .NET 4.x:ään

---
+++ Assets/_Game/Scripts/UI/Bubble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Bubble : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private List<Sprite> emojis;
+    private int counter;
+    private bool animating = false;
+
+    // adjustable values
+    [SerializeField] private float animationTime = 0.5f;
+    [SerializeField] private Sprite initBubble;
+    [SerializeField] private Sprite regularBubble;
+
+    void Start() 
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void Initialize(List<Sprite> sprites, Vector3 startPos, float xOffset)
+    {
+        emojis = sprites;
+        counter = emojis.Count;
+        gameObject.transform.position = startPos;
+    }
+
+
+}
