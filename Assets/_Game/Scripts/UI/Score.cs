d0e960a2246b6cc57178250c502a1c104448c123
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2020-02-01 21:59:02 +0200

    Create score calculation.

---
+++ Assets/_Game/Scripts/UI/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private string id;
+
+    private void OnEnable()
+    {
+        int score = PlayerPrefs.GetInt(id, 0);
+        int index = 0;
+        foreach(Transform child in transform)
+        {
+            child.GetChild(0).gameObject.SetActive(index < score);
+            index++;
+        }
+    }
+}
